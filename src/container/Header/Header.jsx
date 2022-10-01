905da2d2e31b85113d04bd25cbd9bc7b61af2210
AllyHussein <alihussein95@outlook.com>
2022-10-01 16:44:41 +0200

    restaurant

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { SubHeading } from '../../components';
+import images from '../../constants/images';
+import './Header.css';
+
+const Header = () => (
+  <div className='app__header app__wrapper section__padding' id='home'>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase The New Flavour" />
+      <h1 className='app__header-h1'>The Key To Fine Dinning</h1>
+      <p className='p__opensans' style={{ margin: "2rem 0" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        optio, eaque rerum!</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header' />
+
+    </div>
+
+  </div>
+);
+
+export default Header;
